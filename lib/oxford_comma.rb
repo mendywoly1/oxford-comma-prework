7ce212851946a256ee8698c70c96112a8be311a4
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-04-20 02:30:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,5 @@
 def oxford_comma(array)
-  new_string = ""
-  if array.size > 2
-    new_string << array[0] + "and" array[1]
-  else 
-    string = array.join(" and ")
-  end
+  if array.size < 2 
+      string = array.join(" and ")
+  end 
 end
