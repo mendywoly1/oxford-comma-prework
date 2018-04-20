64aaf073e46dcd4891de3aed15f791aa241aa8c5
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-04-20 02:55:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size < 3 
+      string = array.join(" and ")
+  else 
+    string = ""
+    array.each_with_index do |val, index|
+      if index == array.length - 1 
+        string << "and #{val}"
+      else
+        string << "#{val}, "
+      end
+    end
+    string
+  end
 end
