33f930d8d8619c2a8b124fba29b52b1f64c21a05
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-04-20 02:48:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,13 +2,6 @@ def oxford_comma(array)
   if array.size < 3 
       string = array.join(" and ")
   else 
-     counter = 1
-     string = ""
-         array.each do |val|
-           string << val
-         end
-         puts string
-      end 
-  
-  end 
+    
+  end
 end
